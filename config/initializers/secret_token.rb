9ae27d2fb621f536906893bf8236f9832f62e822
yowana <y.kuvaka.09@aberdeen.ac.uk>
2011-12-12 21:43:25 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WeddingPhotosApp::Application.config.secret_token = 'cd86c28ad74dd04288dc2fb6dc9777ef8af44cf30ad2ac552a069af4da2a2c79cfd3ba3e2ac62e9d0f9cca5afa18182aa40391e80ed103c645caacde08c42cd5'
